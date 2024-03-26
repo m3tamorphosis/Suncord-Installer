3bb7840b1d7b17f0e81d80549237309d0459b7dc
xi <xi@roze.lol>
2024-03-26 19:04:30 +0000

    Formatting fix
    Co-authored-by: nyx <verticalsync@users.noreply.github.com>

--- installer.cpp
+++ installer.cpp
@@ -1,89 +1,91 @@
+#include <filesystem>
 #include <iostream>
 #include <map>
-#include <filesystem>
 #include <string>
 
 // Installers configuration
 
 enum class PACKAGEMANAGER {
-    APT,
-    YUM,
-    EMERGE,
-    PACMAN,
-    ZYPPER,
-    URPMI,
-    NIX_ENV,
-    PKG,
-    PKG_ADD,
-    APK,
-    UNKNOWN,
+  APT,
+  YUM,
+  EMERGE,
+  PACMAN,
+  ZYPPER,
+  URPMI,
+  NIX_ENV,
+  PKG,
+  PKG_ADD,
+  APK,
+  UNKNOWN,
 };
 
-auto PackageManager  = CheckPackageManagers(); 
+auto PackageManager = CheckPackageManagers();
 
 bool IsWindows() {
-  #ifdef _WIN32
-    return true;
-  #else
-    return false;
-  #endif
+#ifdef _WIN32
+  return true;
+#else
+  return false;
+#endif
 }
 
 static std::map<std::string, PACKAGEMANAGER> managers = {
-    {"apt", PACKAGEMANAGER::APT},
-    {"yum", PACKAGEMANAGER::YUM},
-    {"emerge", PACKAGEMANAGER::EMERGE},
-    {"pacman", PACKAGEMANAGER::PACMAN},
-    {"zypper", PACKAGEMANAGER::ZYPPER},
-    {"urpmi", PACKAGEMANAGER::URPMI},
-    {"nix-env", PACKAGEMANAGER::NIX_ENV},
-    {"pkg", PACKAGEMANAGER::PKG},
-    {"pkg_add", PACKAGEMANAGER::PKG_ADD},
-    {"apk", PACKAGEMANAGER::APK}
-};
+    {"apt", PACKAGEMANAGER::APT},         {"yum", PACKAGEMANAGER::YUM},
+    {"emerge", PACKAGEMANAGER::EMERGE},   {"pacman", PACKAGEMANAGER::PACMAN},
+    {"zypper", PACKAGEMANAGER::ZYPPER},   {"urpmi", PACKAGEMANAGER::URPMI},
+    {"nix-env", PACKAGEMANAGER::NIX_ENV}, {"pkg", PACKAGEMANAGER::PKG},
+    {"pkg_add", PACKAGEMANAGER::PKG_ADD}, {"apk", PACKAGEMANAGER::APK}};
 
 PACKAGEMANAGER CheckPackageManagers() {
-    std::string PM[] = {"apt", "yum", "emerge", "pacman", "zypper", "urpmi", "nix-env", "pkg", "pkg_add","apk"};
+  std::string PM[] = {"apt",   "yum",     "emerge", "pacman",  "zypper",
+                      "urpmi", "nix-env", "pkg",    "pkg_add", "apk"};
 
-    for(const std::string& manager : PM) {
-        if(VerifyManager(manager)) {
-          auto PM = managers.find(manager);
-          if(PM != managers.end()) {
-            return PM->second;
-          }
-        }
+  for (const std::string &manager : PM) {
+    if (VerifyManager(manager)) {
+      auto PM = managers.find(manager);
+
+      if (PM != managers.end()) {
+        return PM->second;
+      }
     }
-    
-    printf("Could not find available package managers, please install git manually \n");
-    return PACKAGEMANAGER::UNKNOWN;
+  }
+
+  printf("Could not find available package managers, please install git "
+         "manually \n");
+  return PACKAGEMANAGER::UNKNOWN;
 }
 
-bool VerifyManager(const std::string& manager) {
-    std::string command = "which " + manager;
-    int result = std::system(command.c_str());
-    return (result == 0);
+bool VerifyManager(const std::string &manager) {
+  std::string command = "which " + manager;
+  int result = std::system(command.c_str());
+  return (result == 0);
 }
+
+// Installation
 
 void InstallGit() {
   int result;
-  std::filesystem::path cache = std::filesystem::temp_directory_path() / "git-cache";
-  
+  std::filesystem::path cache =
+      std::filesystem::temp_directory_path() / "git-cache";
+
   printf("Creating git-cache directory\n");
 
-  if(!std::filesystem::exists(cache)) {
+  if (!std::filesystem::exists(cache)) {
     std::filesystem::create_directory(cache);
   }
 
   printf("Installing Git...\n");
 
-  if(IsWindows()) {
-    result = std::system("curl -o Git-Installer.exe https://github.com/git-for-windows/git/releases/download/v2.44.0.windows.1/Git-2.44.0-64-bit.exe");
+  if (IsWindows()) {
+    result = std::system("curl -o Git-Installer.exe "
+                         "https://github.com/git-for-windows/git/releases/"
+                         "download/v2.44.0.windows.1/Git-2.44.0-64-bit.exe");
 
-    if(result == 0) {
+    if (result == 0) {
       printf("Downloaded Git installer\n");
       result = std::system("git-cache/Git-Installer.exe");
 
-      if(result == 0) {
+      if (result == 0) {
         printf("Git installed\n");
       }
     }
@@ -97,82 +99,83 @@ void InstallGit() {
     printf("Failed to download git installer\n");
   }
 
-  if(!IsWindows()) {
+  if (!IsWindows()) {
     InstallLinuxDependency("git");
   }
 }
 
 void InstallNPM() {
   int result;
-  std::filesystem::path cache = std::filesystem::temp_directory_path() / "npm-cache";
-  
+  std::filesystem::path cache =
+      std::filesystem::temp_directory_path() / "npm-cache";
+
   printf("Creating npm-cache directory\n");
 
-  if(!std::filesystem::exists(cache)) {
+  if (!std::filesystem::exists(cache)) {
     std::filesystem::create_directory(cache);
   }
 
   printf("Installing NPM...\n");
 
-  if(IsWindows()) {
-    result = std::system("curl -o npm-installer.msi https://nodejs.org/dist/v21.7.1/node-v21.7.1-x64.msi"); 
-    if(result == 0) {
+  if (IsWindows()) {
+    result =
+        std::system("curl -o npm-installer.msi "
+                    "https://nodejs.org/dist/v21.7.1/node-v21.7.1-x64.msi");
+
+    if (result == 0) {
       printf("Downloaded NPM installer\n");
       result = std::system("npm-cache/npm-installer.msi");
-      if(result == 0) {
+
+      if (result == 0) {
         printf("Installed NPM\n");
-      }
-      else {
+      } else {
         printf("Failed to install NPM, please install manually\n");
       }
-    }
-    else {
+    } else {
       printf("Failed to download NPM installer\n");
     }
   }
 
-  if(!IsWindows()) {
+  if (!IsWindows()) {
     InstallLinuxDependency("npm");
   }
 }
 
-void InstallPNPM() {
-  system("npm i -g pnpm");
-}
+void InstallPNPM() { system("npm i -g pnpm"); }
 
 void InstallLinuxDependency(std::string dep) {
-    printf("Using %s", PackageManager);
-    
-    switch(PackageManager) {
-      case PACKAGEMANAGER::APK:
-        std::system(("sudo apk add " + dep).c_str());
-        break;
-      case PACKAGEMANAGER::APT:
-        std::system(("sudo apt-get install " + dep).c_str());
-        break;
-      case PACKAGEMANAGER::EMERGE:
-        std::system(("sudo emerge --ask --verbose " + dep).c_str());
-        break;
-      case PACKAGEMANAGER::NIX_ENV:
-        std::system(("sudo nix-env -i " + dep).c_str());
-        break;
-      case PACKAGEMANAGER::PACMAN:
-        std::system(("sudo pacman -S " + dep).c_str());
-        break;
-      case PACKAGEMANAGER::PKG:
-        std::system(("sudo pkg install " + dep).c_str());
-        break;
-      case PACKAGEMANAGER::PKG_ADD:
-        std::system(("sudo pkg_add " + dep).c_str());
-        break;
-      case PACKAGEMANAGER::URPMI:
-        std::system(("sudo urmpi " + dep).c_str());
-        break;
-      case PACKAGEMANAGER::YUM:
-        std::system(("sudo yum install " + dep).c_str());
-        break;
-      case PACKAGEMANAGER::ZYPPER:
-        std::system(("sudo zypper install " + dep).c_str());
-        break;
-    }
+  printf("Using %s", PackageManager);
+
+  switch (PackageManager) {
+  case PACKAGEMANAGER::APK:
+    std::system(("sudo apk add " + dep).c_str());
+    break;
+  case PACKAGEMANAGER::APT:
+    std::system(("sudo apt-get install " + dep).c_str());
+    break;
+  case PACKAGEMANAGER::EMERGE:
+    std::system(("sudo emerge --ask --verbose " + dep).c_str());
+    break;
+  case PACKAGEMANAGER::NIX_ENV:
+    std::system(("sudo nix-env -i " + dep).c_str());
+    break;
+  case PACKAGEMANAGER::PACMAN:
+    std::system(("sudo pacman -S " + dep).c_str());
+    break;
+  case PACKAGEMANAGER::PKG:
+    std::system(("sudo pkg install " + dep).c_str());
+    break;
+  case PACKAGEMANAGER::PKG_ADD:
+    std::system(("sudo pkg_add " + dep).c_str());
+    break;
+  case PACKAGEMANAGER::URPMI:
+    std::system(("sudo urmpi " + dep).c_str());
+    break;
+  case PACKAGEMANAGER::YUM:
+    std::system(("sudo yum install " + dep).c_str());
+    break;
+  case PACKAGEMANAGER::ZYPPER:
+    std::system(("sudo zypper install " + dep).c_str());
+    break;
+  }
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
+#include "installer.cpp"
 #include <iostream>
 #include <unistd.h>
-#include "installer.cpp"
 
 // Suncord Github: https://github.com/verticalsync/Suncord
 // My contact: @xbhl
@@ -9,12 +9,14 @@
 // Any issues I urge you make an issue report in github.
 
 bool CheckGit() {
-  printf("Checking if GIT is installed\n");
+  printf("Checking if GIT is installed \n");
   int result = std::system("git --version");
-  if(result == 0) {
-    printf("Git is installed\n");
+
+  if (result == 0) {
+    printf("Git is installed \n");
     return true;
   }
+
   else {
     printf("Git is not installed\n");
     return false;
@@ -24,90 +26,89 @@ bool CheckGit() {
 bool CheckNPM() {
   printf("Checking for NPM");
   int result = std::system("npm -v");
-  if(result == 0) {
-    printf("NPM is installed\n");
+
+  if (result == 0) {
+    printf("NPM is installed \n");
     return true;
   }
+
   else {
-    printf("NPM is not installed\n");
+    printf("NPM is not installed \n");
     return false;
   }
 }
 
 bool CheckPNPM() {
-  printf("Checking for PNPM\n");
+  printf("Checking for PNPM \n");
   int result = std::system("pnpm --version");
-  if(result == 0) {
-    printf("PNPM is installed\n");
+
+  if (result == 0) {
+    printf("PNPM is installed \n");
     return true;
   }
-  else {
-    printf("PNPM is not installed\n");
-    return false;
-  }
+
+  else
+    printf("PNPM is not installed \n");
+  return false;
 }
 
 void Build() {
-    printf("Cloning Suncord repository...\n");
-    int result = system("git clone https://github.com/verticalsync/Suncord");
+  printf("Cloning Suncord repository... \n");
+  int result = system("git clone https://github.com/verticalsync/Suncord");
 
-    if (result == 0) {
-        printf("Cloned Suncord repository.\n");
-        if (chdir("Suncord") == 0) {
-            printf("Opened repository.\nDownloading external dependencies using PNPM...\n");
-            result = system("pnpm install --frozen-lockfile");
+  if (result != 0) {
+    printf("Failed to clone repository. \n");
+    return;
+  }
 
-            if (result == 0) {
-                printf("Installed dependencies!\nBuilding Suncord...\n");
-                result = system("pnpm build");
+  if (chdir("Suncord") != 0) {
+    printf("Couldn't open Suncord directory");
+    return;
+  }
+  printf("Opened Suncord directory!\nInstalling external dependencies using "
+         "PNPM...\n");
 
-                if (result == 0) {
-                    printf("Built Suncord.\nInjecting...\n");
-                    result = system("pnpm inject");
+  result = system("pnpm install --frozen-lockfile");
+  if (result != 0) {
+    printf("Failed to install frozen lockfile dependencies.\n");
+    return;
+  }
+  printf("Installed frozen lockfile dependencies\nBeginnning build...\n");
 
-                    if (result == 0) {
-                        printf("Finished injecting!\n");
-                        printf("Press any key to exit.\n");
-                        getchar();
-                    } else {
-                        printf("Failed to inject to Discord client\n");
-                    }
-                } else {
-                    printf("Failed to build Suncord\n");
-                }
-            } else {
-                printf("Failed to install PNPM dependencies.\n");
-            }
-        } else {
-            printf("Couldn't open Suncord directory.\n");
-        }
-    } else {
-        printf("Failed to clone repository\n");
-    }
+  result = system("pnpm bulid");
+  if (result != 0) {
+    printf("Failed to build Suncord.\n");
+    return;
+  }
+  printf("Successfully built Suncord! \n");
+
+  result = system("pnpm inject");
+  if (result != 0) {
+    printf("Failed to inject to Discord \n");
+    return;
+  }
+  printf("Successfully injected!\nPress any key to exit.");
+  getchar();
 }
 
 int main() {
-  printf("Performing checks\n");
-  
+  printf("Performing checks \n");
+
   bool GitInstalled = CheckGit();
   bool NPMInstalled = CheckNPM();
   bool PNPMInstalled = CheckPNPM();
 
-  if(!GitInstalled) {
+  if (!GitInstalled)
     InstallGit();
-  }
 
-  if(!NPMInstalled) {
+  if (!NPMInstalled)
     InstallNPM();
-  }
 
-  if(!PNPMInstalled && NPMInstalled) {
+  if (!PNPMInstalled && NPMInstalled)
     InstallPNPM();
-  }
 
-  if(GitInstalled && NPMInstalled && PNPMInstalled) {
+  if (GitInstalled && NPMInstalled && PNPMInstalled)
     Build();
-  }
 
-   return 0;
+  return 0;
 }
